78abf107f8b8c05ab312d6d04bc2d2120ad3d345
Yousif <meemeemeemeem@protonmail.com>
2021-06-08 16:42:01 +0100

    Improved printing of final.py and added a measure of prediction probability

--- final.py
+++ final.py
@@ -1,11 +1,13 @@
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
 
 from importer import X_orig_std, X_jaffa_std, y  # get the full datasets and jaffa datasets from importer.py
 
 # define our models according to the best performers from classifier.py
-forest_model = RandomForestClassifier(n_estimators=80, class_weight="balanced")
-boost_model = GradientBoostingClassifier(loss="exponential", n_estimators=240, learning_rate=0.15, max_depth=7)
+forest_model = RandomForestClassifier(n_estimators=80, class_weight="balanced", random_state=0)
+boost_model = GradientBoostingClassifier(loss="exponential", n_estimators=240, learning_rate=0.15, max_depth=7,
+                                         random_state=0)
 knn_model = KNeighborsClassifier(n_neighbors=22, weights="distance", algorithm="ball_tree", p=2)
 
 # fit our models to the whole dataset of all cakes and biscuits
@@ -19,10 +21,34 @@ boost_predictions = boost_pred_model.predict(X_jaffa_std)
 knn_predictions = knn_pred_model.predict(X_jaffa_std)
 
 # augment data with added recipe numbers for added insight in conjunction with the graph
-forest_predictions_print = [(forest_predictions[i], i+1) for i in range(12)]
-boost_predictions_print = [(boost_predictions[i], i+1) for i in range(12)]
-knn_predictions_print = [(knn_predictions[i], i+1) for i in range(12)]
+predict_df = pd.DataFrame(
+    {"Recipe No.": list(range(1, 13)),
+     "Forest": forest_predictions,
+     "Boosting": boost_predictions,
+     "KNN": knn_predictions,
+}
+)
 
 # print the predictions with the recipe numbers
-print("Forest", forest_predictions_print, "\nBoosting", boost_predictions_print,
-      "\nKNN", knn_predictions_print)
+with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+    print(predict_df)
+
+# find the probabilities for each class prediction for more insight
+forest_predictions_probs = forest_pred_model.predict_proba(X_jaffa_std)
+boost_predictions_probs = boost_pred_model.predict_proba(X_jaffa_std)
+knn_predictions_probs = knn_pred_model.predict_proba(X_jaffa_std)
+
+# put data into DataFrame for easier visualisation
+prob_df = pd.DataFrame(
+    {"Recipe No.": list(range(1, 13)),
+     "Forest p_biscuit": ["{:.4f}".format(float(i[0])) for i in forest_predictions_probs],
+     "Forest p_cake": ["{:.4f}".format(float(i[1])) for i in forest_predictions_probs],
+     "Boosting p_biscuit": ["{:.4f}".format(float(i[0])) for i in boost_predictions_probs],
+     "Boosting p_cake": ["{:.4f}".format(float(i[1])) for i in boost_predictions_probs],
+     "KNN p_biscuit": ["{:.4f}".format(float(i[0])) for i in knn_predictions_probs],
+     "KNN p_cake": ["{:.4f}".format(float(i[1])) for i in knn_predictions_probs]}
+)
+
+# print the prediction probabilities with the recipe numbers
+with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', 200):
+    print(prob_df)
