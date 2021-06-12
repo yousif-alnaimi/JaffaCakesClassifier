fe6edb50e519e2b91d47c5a94cc04a706c97db4e
Yousif <meemeemeemeem@protonmail.com>
2021-06-12 19:22:40 +0100

    Add separate accuracy score metric

--- classifier.py
+++ classifier.py
@@ -1,7 +1,7 @@
 from sklearn import svm
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.tree import DecisionTreeClassifier
@@ -46,7 +46,7 @@ print(scores3.mean(), scores3.std(), "Boosting")
 # perf_df = pd.DataFrame({"Mean": [i[0] for i in perf_list], "Standard Deviation": [i[1] for i in perf_list],
 #                         "K": [i[2] for i in perf_list]})
 #
-# # intitalise figure
+# # initialise figure
 # fig, ax = plt.subplots()
 #
 # # set x and y labels and colours
@@ -56,7 +56,7 @@ print(scores3.mean(), scores3.std(), "Boosting")
 # # plot a scatter plot of the means in red
 # ax.scatter(perf_df["K"], perf_df["Mean"], color='tab:red')
 #
-# # intialise second y axis for standard deviation
+# # initialise second y axis for standard deviation
 # ax2 = ax.twinx()
 #
 # # initialise colour parameters and labels
@@ -104,40 +104,40 @@ pred_model1 = model1.fit(X_train_std, y_train_std)
 y_pred1 = pred_model1.predict(X_test_std)
 # show the statistics more clearly in console
 print("LR", classification_report(y_test_std, y_pred1, labels=["biscuit", "cake"]),
-      confusion_matrix(y_test_std, y_pred1, labels=["biscuit", "cake"]))
+      confusion_matrix(y_test_std, y_pred1, labels=["biscuit", "cake"]), "\n", accuracy_score(y_test_std, y_pred1))
 
 # random forest classification test
 pred_model2 = model2.fit(X_train_std, y_train_std)
 y_pred2 = pred_model2.predict(X_test_std)
 print("Forest", classification_report(y_test_std, y_pred2, labels=["biscuit", "cake"]),
-      confusion_matrix(y_test_std, y_pred2, labels=["biscuit", "cake"]))
+      confusion_matrix(y_test_std, y_pred2, labels=["biscuit", "cake"]), "\n", accuracy_score(y_test_std, y_pred2))
 
 # boosting classification test
 pred_model3 = model3.fit(X_train_std, y_train_std)
 y_pred3 = pred_model3.predict(X_test_std)
 print("Boost", classification_report(y_test_std, y_pred3, labels=["biscuit", "cake"]),
-      confusion_matrix(y_test_std, y_pred3, labels=["biscuit", "cake"]))
+      confusion_matrix(y_test_std, y_pred3, labels=["biscuit", "cake"]), "\n", accuracy_score(y_test_std, y_pred3))
 
 # KNN classification test
 pred_model4 = model4.fit(X_train_std, y_train_std)
 y_pred4 = pred_model4.predict(X_test_std)
 print("KNN", classification_report(y_test_std, y_pred4, labels=["biscuit", "cake"]),
-      confusion_matrix(y_test_std, y_pred4, labels=["biscuit", "cake"]))
+      confusion_matrix(y_test_std, y_pred4, labels=["biscuit", "cake"]), "\n", accuracy_score(y_test_std, y_pred4))
 
 # Tree classification test
 pred_model5 = model5.fit(X_train_std, y_train_std)
 y_pred5 = pred_model5.predict(X_test_std)
 print("Tree", classification_report(y_test_std, y_pred5, labels=["biscuit", "cake"]),
-      confusion_matrix(y_test_std, y_pred5, labels=["biscuit", "cake"]))
+      confusion_matrix(y_test_std, y_pred5, labels=["biscuit", "cake"]), "\n", accuracy_score(y_test_std, y_pred5))
 
 # SVM classification test
 pred_model6 = model6.fit(X_train_std, y_train_std)
 y_pred6 = pred_model6.predict(X_test_std)
 print("SVM", classification_report(y_test_std, y_pred6, labels=["biscuit", "cake"]),
-      confusion_matrix(y_test_std, y_pred6, labels=["biscuit", "cake"]))
+      confusion_matrix(y_test_std, y_pred6, labels=["biscuit", "cake"]), "\n", accuracy_score(y_test_std, y_pred6))
 
 # LDA classification test
 pred_model7 = model7.fit(X_train_std, y_train_std)
 y_pred7 = pred_model7.predict(X_test_std)
 print("LDA", classification_report(y_test_std, y_pred7, labels=["biscuit", "cake"]),
-      confusion_matrix(y_test_std, y_pred7, labels=["biscuit", "cake"]))
+      confusion_matrix(y_test_std, y_pred7, labels=["biscuit", "cake"]), "\n", accuracy_score(y_test_std, y_pred7))
